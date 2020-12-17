8faa4afabe16e6ac7183d5d110cf72f1379a4374
yamkim <coramdeo0708@gmail.com>
2020-12-17 20:03:48 +0900

    [FE] add register post function.

--- oasis_helper/main_fe.js
+++ oasis_helper/main_fe.js
@@ -19,6 +19,7 @@ app.get('*', function(request, response, next){
   });
 });
 
+/*
 var db = mysql.createConnection({
   host:'localhost',
   user:'nodejs',
@@ -34,6 +35,7 @@ db.query(`select * from beverage`, function(error, topics){
   for (let i = 0; i < topics.length; i++)
     pathList.push(topics[i]);
 }); 
+*/
 
 app.get('/', function(request, response) { 
   var cssPath = "/stylesheets/info_style.css";
@@ -59,8 +61,21 @@ app.get('/beverage', function(request, response) {
 });
 
 app.get('/register', function(request, response) {
-  var cssPath = "/stylesheets/register_style.css";
-  var body = template.register(cssPath, "button");
+  //var cssPath = "/stylesheets/register_style.css";
+  //var body = template.register(cssPath, "button");
+  var body = `
+  <form action="/register_process" method="post">
+    <select class="category" name="category">
+      <option name="beverage" value="1">음료</option>
+      <option name="snack" value="2">간식</option>
+      <option name="etc" value="3">비품</option>
+    </select>
+    <p><input type="text" name="intraId" placeholder="intra ID"></p>
+    <p><textarea name="message" placeholder="message"></textarea></p>
+    <p><input type="submit"></p>
+  </form>
+  `;
+  
   var html = template.html(
     "",
     body,
@@ -68,6 +83,15 @@ app.get('/register', function(request, response) {
   );
   response.send(html);
 })
+
+app.post('/register_process', function(request, response){
+  var category = request.body.category;
+  var intraId = request.body.intraId;
+  var message = request.body.message;
+  console.log(category, intraId, message);
+  response.redirect(`/register`);
+  response.end();
+});
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
---
+++ oasis_helper/test.js
@@ -0,0 +1,24 @@
+var ret = 
+    `
+    <!doctype html>
+    <html>
+    <head>
+        ${head}
+    </head>
+    <script>
+    function test() {
+        console.log('asdasd');
+    }
+    </script>
+    <body>
+
+    <FORM>
+        <input type = 'button' value = '버튼' onclick='test()'>
+
+        </button>
+    </FORM>
+    </body>
+    <tail>
+    </tail>
+    </html>
+    `;
